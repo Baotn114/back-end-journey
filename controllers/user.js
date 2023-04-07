3cfe9cd0a91e4b344835b0a67630a9c7f6e5f3b3
nhokcherry <baosuper456@gmail.com>
2023-04-07 11:57:43 +0700

    add sign-in condition final

--- controllers/user.js
+++ controllers/user.js
@@ -81,21 +81,23 @@ const signin = async (req, res)=>{
 
     //Kiem tra xem user da dc dang ky chua
     const user = await User.findOne({email});
-    const passwords = await bcrypt.compare(password, user.password);
-    
+
     if(!email || !password){
-        res.status(400).send({error: "please fill all the input to sign in!"})
+        res.status(400).send({error: "please fill all fields to continue!"});
     }else if(!user){
-        res.status(400).send({error: 'User is not registerd!'});
-    }else if(!passwords){
-        res.status(400).send({error: "Password is not correct!"})
-    }else if(user && passwords){
-        res.json({
-            _id: user.id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id)
-        })
+        res.status(400).send({error: "User is not registerd or Invalid user"})
+    }else if(user){
+        const passwords = await bcrypt.compare(password, user.password);
+        if(passwords){
+            res.json({
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id)
+            })
+        }else{
+            res.status(400).send({error: "Password is not correct!"})
+        }
     }
 }
 
